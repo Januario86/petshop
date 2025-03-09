d755afc3494d4ac66c0492f6c7040c0da55b6c08
Januario <januario86fp@gmail.com>
2025-03-09 17:20:19 -0300

    fix: Inclusão das annotations de entidade para referencia do ORM

--- src/main/java/com/petshop/entity/Pets.java
+++ src/main/java/com/petshop/entity/Pets.java
@@ -2,12 +2,14 @@ package com.petshop.entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+@Entity
 public class Pets {
 
 	@Id
